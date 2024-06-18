ccd01bbd809281dfaccc84b8515271b5182f2c68
egami <c0a2302451@edu.tue.ac.jp>
2024-06-18 17:00:25 +0900

    演習2:途中

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -38,7 +38,9 @@ def check_bound(rct:pg.Rect) -> tuple[bool, bool]:
         tate = False
     return yoko, tate
 
-def push_rotate():
+
+
+def push_rotate(): 
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_img01 = pg.transform.flip(kk_img, True, False) 
     return {  # 回転辞書
@@ -52,7 +54,29 @@ def push_rotate():
         (+5, -5):pg.transform.rotozoom(kk_img01, 45, 1.0),
         ( 0,  0):kk_img,
 }
+
+
+def bb_big(): #拡大爆弾Surface
+    a = []
+    b = []
+    for r in range(1, 11):
+
+        bomb_img = pg.Surface((20*r, 20*r))
+        pg.draw.circle(bomb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        a.append(r)
+        b.append(bomb_img)
+    return a,b
         
+
+def gameover(screen, kk_rct):
+    font = pg.font.Font(None, 100)
+    text = font.render("Game over", True, (255, 0, 0))
+    text_rct = text.get_rect(center=(WIDTH/2,HEIGHT/2))
+    black = pg.Surface((WIDTH,HEIGHT))
+    black.set_alpha(128)
+    black.fill(0, 0, 0)
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -62,8 +86,10 @@ def main():
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
     bomb_img = pg.Surface((20, 20))  # 爆弾
-    bomb_img.set_colorkey((0, 0, 0))
     pg.draw.circle(bomb_img, (255, 0, 0), (10, 10), 10)
+    # bomb_imgs = bb_big()[1]
+    # bomb_accs = bb_big()[0]
+    bomb_img.set_colorkey((0, 0, 0))
     bomb_rct = bomb_img.get_rect()
     bomb_rct.center = random.randint(0, 1200), random.randint(0, 700)
     vx, vy = +5, +5  # 横、縦の速さ
@@ -75,7 +101,7 @@ def main():
             if event.type == pg.QUIT: 
                 return
         if kk_rct.colliderect(bomb_rct):  # 衝突判定
-            return 
+            return gameover(screen, kk_rct)
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
@@ -85,13 +111,19 @@ def main():
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
 
-        kk_img = kk_imgs[tuple(sum_mv)]
+        kk_img = kk_imgs[tuple(sum_mv)] #こうかとんの動きによって向きが変わる。
 
         kk_rct.move_ip(sum_mv)  # こうかとん画面内
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
 
         screen.blit(kk_img, kk_rct)
+
+        # avx = vx*bomb_accs[min(tmr//500, 9)]
+        # avy = vy*bomb_accs[min(tmr//500, 9)]
+        # bomb_img = bomb_imgs[min(tmr//500, 9)]
+
+        # vx,vy = avx, avy
 
         bomb_rct.move_ip(vx, vy)  # 爆弾画面内
         yoko, tate = check_bound(bomb_rct)
@@ -104,10 +136,12 @@ def main():
         pg.display.update()
         tmr += 1
         clock.tick(50)
+        
 
 
 if __name__ == "__main__":
     pg.init()
     main()
+    print(bb_big())
     pg.quit()
     sys.exit()
